28291631b8fe287a4639f58d7456e87f3ea232b9
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-05-04 14:48:43 +0800

    Initial commit

---
+++ Assets/My/DisplacesScrollList/Scripts/DisplacesScrollList.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacesScrollList : MonoBehaviour
+{
+    public MessageItem messageItemPrefab;
+
+    private string[] messgageArray = new string[50];
+ 
+
+    private MessageItem[] messageItemArray;
+
+    private void Awake()
+    {
+        InitMessage();
+        CreateDefaultMessageItem();
+        InitEvent();
+    }
+
+    private void InitMessage()
+    {
+        for (int i = 0; i < messgageArray.Length; i++)
+        {
+            messgageArray[i] = i.ToString();
+        }
+    }
+
+    private void InitEvent()
+    {
+        if(messgageArray.Length>1)
+        {
+            var content = transform.GetComponent<UIWrapContent>();
+            content.onInitializeItem += DoEvent;
+            content.minIndex = -messgageArray.Length + 1;
+            content.maxIndex = 0;
+            content.enabled = true;
+        }
+    }
+
+    private void CreateDefaultMessageItem()
+    {
+        Vector3 vec3 = new Vector3(0, -150, 0);
+        int length = messgageArray.Length < 10 ? messgageArray.Length : 10;
+        messageItemArray = new MessageItem[length];
+        for (int i = 0; i < length; i++)
+        {
+            messageItemArray[i] = Instantiate(messageItemPrefab, Vector3.zero
+                , Quaternion.identity, transform);
+            messageItemArray[i].transform.localPosition = vec3 * i;
+        }
+    }
+
+    private void DoEvent(GameObject go, int wrapIndex, int realIndex)
+    {
+        int index = -realIndex;
+        if (index >= 0&& index< messgageArray.Length)
+        {
+            messageItemArray[wrapIndex].SetInfo(messgageArray[index]);
+        }
+    }
+}
---
+++ Assets/My/DisplacesScrollList/Scripts/MessageItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageItem : MonoBehaviour
+{
+    public void SetInfo(string i)
+    {
+        transform.Find("MessageInfo/MessageType").GetComponent<UILabel>().text = "消息类型：" + i;
+        transform.Find("MessageInfo/MessageTitle").GetComponent<UILabel>().text = "标题：+" + i;
+        transform.Find("MessageInfo/MessageContent").GetComponent<UILabel>().text = "内容：" + i;
+    }
+}
