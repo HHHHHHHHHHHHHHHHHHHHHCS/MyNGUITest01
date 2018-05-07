0606a92375776b26a85de607aacdf04208e5f69d
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-05-07 16:55:32 +0800

    3D2D的人物展示面板

---
+++ Assets/My/3DAnd2DDisplay/Scripts/HeroDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetTS;
+
+    private void Awake()
+    {
+        UIEventListener linstener = UIEventListener.Get(gameObject);
+        linstener.onDrag += ScrollHero;
+    }
+
+    private void ScrollHero(GameObject go, Vector2 delta)
+    {
+        if (delta.x != 0)
+        {
+            targetTS.localEulerAngles += (delta.x > 0 ? -1 : 1) * Vector3.up * 180 * Time.deltaTime;
+        }
+    }
+}
