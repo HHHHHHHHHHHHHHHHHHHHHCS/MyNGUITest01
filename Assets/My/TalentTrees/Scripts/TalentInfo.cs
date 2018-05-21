f156cd6bddd99cff640740a540f016f9a1d14b7a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-05-21 17:46:50 +0800

    天赋树的创建

---
+++ Assets/My/TalentTrees/Scripts/TalentInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalentInfo
+{
+    public int SkillID { get; set; }
+    public int SkillName { get; set; }
+    public int SkillSprite { get; set; }
+    public int SkillConditionLevel { get; set; }
+    public int SkillSkillLevel { get; set; }
+    public int SkillMaxLevel { get; set; }
+    public KeyValuePair<int, int>[] SkillCondition { get; set; }
+}
---
+++ Assets/My/TalentTrees/Scripts/TalentInfoManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalentInfoManager : MonoBehaviour
+{
+    private Dictionary<int, TalentInfo> skillInfoDic;
+
+    private void Awake()
+    {
+        skillInfoDic = new Dictionary<int, TalentInfo>();
+    }
+}
