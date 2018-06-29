c64fc5645e9314257d928d3469b61cf059a7c732
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-06-29 16:05:54 +0800

    3D 摄像机滚动的BUG 修复

--- Assets/My/3DCameraScroll/Scripts/CameraScrollBy3D.cs
+++ Assets/My/3DCameraScroll/Scripts/CameraScrollBy3D.cs
@@ -3,34 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 3D世界摄像机事件
+/// 包括移动和点击射线
+/// </summary>
 public class CameraScrollBy3D : MonoBehaviour
 {
-    public Text buildName;
+    private Transform targetTS;//要移动的目标
+    private Camera targetCamera;//摄像机 用来做射线检测
 
-    private Transform targetTS;
-    private Camera targetCamera;
+    private readonly Vector3 startCameraPos = new Vector3(-20, 50, -13);//摄像机初始位置
+    private readonly Vector3 startCameraRot = new Vector3(60, 0, 0);//摄像机初始的旋转
+    private readonly Vector2 clampZoomScale = new Vector2(40, 80);//限制摄像机的缩放大小  最小最大
+    private readonly Vector4 nessClampBorder = new Vector4(-45, 205, -100, 60);//滚动的时候边界 左右下上
+    private readonly Vector4 clampClampBorder = new Vector4(-60, 220, -115, 75);//移动的时候边界 左右下上
 
-    private readonly Vector3 startCameraPos = new Vector3(-20, 50, -13);
-    private readonly Vector2 clampZoomScale = new Vector2(40, 80);
-    private readonly Vector4 nessClampBorder = new Vector4(-45, 205, -100, 60);
-    private readonly Vector4 clampClampBorder = new Vector4(-60, 220, -115, 75);
 
-    private const float moveSpeed = 4, scrollDistance = 20, scrollNess = 100
-        , zoomScaleSpeed = 33, borderNess = 100, borderNessTime = 0.2f, borderBackSpeed = 100
-        , moveMaxDistance = 10, baseZoomScale = 60;
-    private const int queMaxNumber = 10;
+    private const float moveSpeed = 4//摄像机的移动速度
+        , scrollDistance = 20//滚动开检测的距离
+        , scrollNess = 100//阻止滚动的力量大小
+        , zoomScaleSpeed = 33//缩放的速度
+        , borderNessTime = 0.25f//超过了阻尼边界返回的时间
+        , borderBackSpeed = 25//边界返回的速度
+        , moveMaxDistance = 10//如果不小心移动了,射线检测的最大距离
+        , baseZoomScale = 60;//摄像机基础的缩放大小
+    private const int queMaxNumber = 10;//队列最大数量
 
-    private float moveDistance;
-    private Vector2 lastPos;
-    private bool isStayTouch, isTwoTouch;
-    private LinkedList<Vector2> vec2List;
+    private GameObject clickObj;//点击的建筑物体
+    private int layermask;//射线检测的tower layermask
+    private float moveDistance;//当前移动的距离
+    private Vector2 lastPos;//最后的位置
+    private bool isStayTouch, isTwoTouch;//是否一直按着,是否两个手指按着
+    private LinkedList<Vector2> vec2List;//记录鼠标移动的位置
     private Vector2 scrollAccele, nessScrollAccele, borderSpeed, nessBorderAccele, oldPos1, oldPos2;
 
     private void Awake()
     {
         targetTS = transform;
+        layermask = LayerMask.GetMask("Tower");
         targetCamera = targetTS.GetComponent<Camera>();
         targetTS.position = startCameraPos;
+        targetTS.eulerAngles = startCameraRot;
         vec2List = new LinkedList<Vector2>();
     }
 
@@ -40,6 +53,10 @@ public class CameraScrollBy3D : MonoBehaviour
         UpdateScrollScreen();
     }
 
+
+    /// <summary>
+    /// 检测touch  是鼠标还是手指  手指是几根
+    /// </summary>
     private void CheckTouch()
     {
 #if UNITY_STANDALONE_WIN || UNITY_EDITOR
@@ -60,8 +77,16 @@ public class CameraScrollBy3D : MonoBehaviour
 #endif
     }
 
+    /// <summary>
+    /// 一个点击事件的时候执行
+    /// </summary>
     private void OnOneTouchEvent()
     {
+        if (UICamera.isOverUI)
+        {
+            return;
+        }
+
         Vector2 nowPos = Input.mousePosition;
         if (isTwoTouch)
         {
@@ -82,6 +107,10 @@ public class CameraScrollBy3D : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 一个手指按下的时候
+    /// </summary>
+    /// <param name="nowPos"></param>
     private void OnePress(Vector2 nowPos)
     {
         lastPos = nowPos;
@@ -92,28 +121,39 @@ public class CameraScrollBy3D : MonoBehaviour
         moveDistance = 0;
         vec2List.Clear();
         AddQueue(nowPos);
+        StartRayCast(nowPos, true);
     }
 
+    /// <summary>
+    /// 一个手指抬起的时候
+    /// </summary>
+    /// <param name="nowPos"></param>
     private void OneUp(Vector2 nowPos)
     {
         isStayTouch = false;
         AddQueue(nowPos);
         MoveDir(Vector2.zero, false, true);
-        if (!StartRayCast(nowPos))
+        if (!StartRayCast(nowPos, false))
         {
             ScrollScreen();
         }
     }
 
+    /// <summary>
+    /// 一个手指持续在屏幕上的时候
+    /// </summary>
+    /// <param name="nowPos"></param>
     private void OneStay(Vector2 nowPos)
     {
-
         AddQueue(nowPos);
         Vector2 newVec2 = nowPos - lastPos;
         MoveDir(newVec2);
         lastPos = nowPos;
     }
 
+    /// <summary>
+    /// 滚动屏幕
+    /// </summary>
     private void ScrollScreen()
     {
         if (vec2List.Count > 1)
@@ -128,6 +168,10 @@ public class CameraScrollBy3D : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 记录鼠标的位置的队列
+    /// </summary>
+    /// <param name="nowPos"></param>
     private void AddQueue(Vector2 nowPos)
     {
         if (vec2List.Count >= queMaxNumber)
@@ -137,6 +181,9 @@ public class CameraScrollBy3D : MonoBehaviour
         vec2List.AddLast(nowPos);
     }
 
+    /// <summary>
+    /// 执行屏幕滚动
+    /// </summary>
     private void UpdateScrollScreen()
     {
         if (scrollAccele != Vector2.zero)
@@ -179,6 +226,12 @@ public class CameraScrollBy3D : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 移动执行方法
+    /// </summary>
+    /// <param name="dir"></param>
+    /// <param name="isScroll"></param>
+    /// <param name="isUp"></param>
     private void MoveDir(Vector2 dir, bool isScroll = false, bool isUp = false)
     {
         var moveVec3 = -new Vector3(dir.x, 0, dir.y) * Time.deltaTime * moveSpeed * (targetCamera.fieldOfView / baseZoomScale);
@@ -234,6 +287,9 @@ public class CameraScrollBy3D : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 两个手指进行缩放的时候的方法
+    /// </summary>
     private void OnTwoTouchEvent()
     {
         if (scrollAccele != Vector2.zero)
@@ -269,6 +325,10 @@ public class CameraScrollBy3D : MonoBehaviour
         oldPos2 = newTouch2.position;
     }
 
+    /// <summary>
+    /// 屏幕缩放执行方法
+    /// </summary>
+    /// <param name="offset"></param>
     private void ZoomCamera(float offset)
     {
         float scaleFactor = offset / zoomScaleSpeed;
@@ -277,17 +337,31 @@ public class CameraScrollBy3D : MonoBehaviour
         targetCamera.fieldOfView = endView;
     }
 
-    private bool StartRayCast(Vector2 nowPos)
+    /// <summary>
+    /// 射线检测方法
+    /// </summary>
+    /// <param name="nowPos"></param>
+    /// <param name="isFirst"></param>
+    /// <returns></returns>
+    private bool StartRayCast(Vector2 nowPos, bool isFirst)
     {
         if (moveDistance <= moveMaxDistance)
         {
             RaycastHit hitInfo;
             Ray ray = targetCamera.ScreenPointToRay(nowPos);
-            if (Physics.Raycast(ray, out hitInfo, 1000f))
+            if (Physics.Raycast(ray, out hitInfo, 10000f, layermask))
             {
-                GameObject gameObj = hitInfo.collider.gameObject;
-                Debug.Log(gameObj);
-                buildName.text = gameObj.name;
+                var buildObj = hitInfo.collider.gameObject;
+                if (isFirst)
+                {
+                    clickObj = buildObj;
+                }
+                else if (clickObj == buildObj)
+                {
+                    Debug.Log(clickObj);
+                }
+
+
                 return true;
             }
             return false;
